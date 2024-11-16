cb887f437dc374c856f1534f1244d8359771d907
Kmdillenb <kmdillenb@gmail.com>
2024-11-16 14:52:30 -0600

    renamed file

--- TimeTrackerSpringBoot/src/main/java/com/TimeTracker/SwanHacks/TimeTracker/TimeTrackerApplication.java
+++ TimeTrackerSpringBoot/src/main/java/com/TimeTracker/SwanHacks/TimeTracker/TimeTrackerApplication.java
@@ -8,6 +8,7 @@ public class TimeTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimeTrackerApplication.class, args);
+
 	}
 
 }
--- TimeTrackerSpringBoot/src/main/java/com/TimeTracker/SwanHacks/TimeTracker/TimeTrackerController.java
+++ TimeTrackerSpringBoot/src/main/java/com/TimeTracker/SwanHacks/TimeTracker/TimeTrackerController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,21 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TimeTrackerController {
 
-    private Map<String, TimeTracker> db = new HashMap<>() {
-        {
-            put("1", new TimeTracker("1", "hello.jpg"));
-        }
-    };
+    private Map<Integer, Event> timeChart = new HashMap<>();
 
     @GetMapping("/")
     public String hello() {
         return "This Works!";
     }
 
-    @GetMapping("/data")
-    public Collection<TimeTracker> get() {
-        return db.values();
-    }
+    // @GetMapping("/data")
+    // public Collection<TimeTracker> get() {
+    // return db.values();
+    // }
 
     @GetMapping("/data")
     public ResponseEntity<Object> helloWorld() {
@@ -39,19 +36,24 @@ public class TimeTrackerController {
         return ResponseEntity.ok(data);
     }
 
-    @GetMapping("/data/{id}")
-    public TimeTracker get(@PathVariable String id) {
-        TimeTracker photo = db.get(id);
-        if (photo == null)
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-        return photo;
+    @GetMapping("/data/test")
+    public String displayString(@RequestParam String inputString) {
+        return "You entered: " + inputString;
     }
 
-    @DeleteMapping("/data/{id}")
-    public void remove(@PathVariable String id) {
-        TimeTracker photo = db.remove(id);
-        if (photo == null)
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-    }
+    // @GetMapping("/data/{id}")
+    // public TimeTracker get(@PathVariable String id) {
+    // TimeTracker photo = db.get(id);
+    // if (photo == null)
+    // throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    // return photo;
+    // }
+
+    // @DeleteMapping("/data/{id}")
+    // public void remove(@PathVariable String id) {
+    // TimeTracker photo = db.remove(id);
+    // if (photo == null)
+    // throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    // }
 
 }
